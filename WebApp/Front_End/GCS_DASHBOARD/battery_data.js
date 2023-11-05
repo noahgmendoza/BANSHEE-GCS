5ce7fee3cc10db942a08f3b070e8104840424981
decryptec <dhsdon47@outlook>
2023-11-05 01:37:12 -0700

    bat voltage route and table columns

--- WebApp/Front_End/GCS_DASHBOARD/battery_data.js
+++ WebApp/Front_End/GCS_DASHBOARD/battery_data.js
@@ -1,16 +1,20 @@
-fetch("http://149.28.81.138:80/api/data")
+fetch("http://149.28.81.138:80/api/voltage")
     .then((bdata) =>{
         //console.log(data);  
         return bdata.json(); //convert to object
      }).then((objectbData)=>{
-         console.log(objectbData['Sensor data']); //shows data stored in array[0] of "Sensor Data"
+         console.log(objectbData['Voltage Data']); //shows data stored in array[0] of "Sensor Data"
          let btableData="";
          Object.keys(objectbData)
-         objectbData['Sensor data'].map((values)=>{
+         objectbData['Voltage Data'].map((values)=>{
               btableData+=`<tr>
-            <td>${values.mavpackettype}</td>
-            <td>${values.type}</td>
-            
+            <td>${values.BatteryID}</td>
+            <td>${values['Total Voltage']}</td>
+            <td>${values['Cell 1']}</td>
+            <td>${values['Cell 2']}</td>
+            <td>${values['Cell 3']}</td>
+            <td>${values['Cell 4']}</td>
+
             </tr>`
          });
          document.getElementById("btable_body").
