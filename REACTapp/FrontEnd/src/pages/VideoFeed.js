bed726fc01b23de0037a1376af87f8b6009ae61c
Brian Lam <112601782+ishwo0@users.noreply.github.com>
2024-04-14 13:44:43 -0700

    video update 2

--- REACTapp/FrontEnd/src/pages/VideoFeed.js
+++ REACTapp/FrontEnd/src/pages/VideoFeed.js
@@ -5,35 +5,39 @@ function VideoFeed() {
 
 
     const videoRef = useRef(null);
-    let ws;
+    const wsRef = useRef(null);
 
     useEffect(() => {
-    // Establish WebSocket connection when component mounts
-    ws = new WebSocket('ws://172.233.146.163:3000/read_frames');
+        // Establish WebSocket connection when component mounts
+        wsRef.current = new WebSocket('ws://172.233.146.163:3000/read_frames');
 
-    ws.onopen = () => {
-      console.log('Connected to WebSocket server for reading frames');
-    };
+        wsRef.current.onopen = () => {
+            console.log('Connected to WebSocket server for reading frames');
+        };
 
-    ws.onmessage = (event) => {
-      const arrayBuffer = event.data;
-      const blob = new Blob([arrayBuffer], { type: 'image/jpeg' });
-      const imageUrl = URL.createObjectURL(blob);
-      videoRef.current.src = imageUrl;
-    };
+        wsRef.current.onmessage = (event) => {
+            const arrayBuffer = event.data;
+            const blob = new Blob([arrayBuffer], { type: 'image/jpeg' });
+            const imageUrl = URL.createObjectURL(blob);
+            videoRef.current.src = imageUrl;
+        };
 
-    return () => {
-        // Close WebSocket connection when component unmounts
-        ws.close();
-      };
+        return () => {
+            // Close WebSocket connection when component unmounts
+            if (wsRef.current) {
+                wsRef.current.close();
+            }
+        };
     }, []);
 
 
 
   return (
-    <div>
-      <h1>Live Video Stream</h1>
-      <video ref={videoRef} autoPlay playsInline />
+    <div style={{background: 'rgb(54, 58, 69)'}} className='container'>
+        <h1 style={{textAlign: 'center' , color: 'rgb(214, 214, 214)'}}>Live Video Stream</h1>
+        <div style={{ width: '100%', height: 'auto', overflow: 'hidden' }}>
+            <video ref={videoRef} autoPlay playsInline style={{ width: '100%', height: 'auto' }} />
+        </div>
     </div>
   )
 
